896868a8d316177568c009a6a5fe98a0ce63ee43
felipehuicochea <37923616+felipehuicochea@users.noreply.github.com>
2025-07-30 23:15:46 -0500

    Update ApplicationPartsLogger.cs

--- BTCPayServer/Plugins/SimpleSwap/Services/ApplicationPartsLogger.cs
+++ BTCPayServer/Plugins/SimpleSwap/Services/ApplicationPartsLogger.cs
@@ -1,8 +1,8 @@
 using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
